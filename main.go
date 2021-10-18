3ea332822793ec7f3a2da6b047ac396100e2f07c
Simoncao <13671707129@126.com>
2021-10-18 15:12:15 +0800

    [feat] mongo findone

--- main.go
+++ main.go
@@ -56,18 +56,16 @@ func Process(client *mongo.Client, collection *mongo.Collection, line string) {
 	if err != nil {
 		log.Printf("failed to search: %v", err)
 	}
-	/**
 	if err.Error() == "mongo: no documents in result" {
-
+		/**
 		_, errIns := collection.InsertOne(context.TODO(), init)
 		if errIns != nil {
 			log.Printf("failed to insert init: %v\n", errIns)
 		}
 
 		fmt.Println("match")
+		**/
 	}
-	**/
-
 	fmt.Println(result)
 }
 
