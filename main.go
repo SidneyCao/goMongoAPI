b90bff027f6e3a0a68750177649ec064d357962a
Simoncao <13671707129@126.com>
2021-10-18 11:52:14 +0800

    [feat] readlines from file

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -18,10 +20,18 @@ var (
 	password   = flag.String("p", "", "mongo password")
 	db         = flag.String("d", "", "mongo db")
 	collection = flag.String("c", "", "mongo collection")
+	file       = flag.String("f", "", "read file")
 )
 
+func Process(client *mongo.Client, collection *mongo.Collection) {
+
+}
+
 func main() {
+	//获取参数
 	flag.Parse()
+
+	//新建mongo client
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@%s:27017", *user, *password, *host)))
@@ -33,9 +43,25 @@ func main() {
 			log.Panicf("failed to disconnect mongo: %v", err)
 		}
 	}()
+	//获取collection
+	//collection := client.Database(*db).Collection(*collection)
 
-	collection := client.Database(*db).Collection(*collection)
-
+	//读取差异文件
+	f, err := os.Open(*file)
+	if err != nil {
+		log.Panicf("failed to open diff file: %v", err)
+	}
+	defer f.Close()
+	//按行读取文件
+	br := bufio.NewReader(f)
+	for {
+		line, _, err := br.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		fmt.Print(string(line))
+	}
+	/**
 	filter := bson.D{{"anjie", 1}}
 
 	var result bson.D
@@ -45,4 +71,5 @@ func main() {
 	}
 
 	fmt.Println(result)
+	**/
 }
